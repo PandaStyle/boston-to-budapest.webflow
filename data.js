1370c06592e308c92f0a18f4510c8ad1872e3476
zsolt nemeth <furion1@gmail.com>
2016-11-04 15:36:32 -0400

    Initial commit

---
+++ data.js
@@ -0,0 +1,71 @@
+export default  [
+    {
+        num: "01",
+        dest: "Boston",
+        date: "- / 02.01",
+        color1: "#2E3192",
+        color2: "#1BFFFF"
+    },
+    {
+        num: "02",
+        dest: "LA - SF",
+        date: "02.01 / 02.08",
+        color1: "#D4145A",
+        color2: "#FBB03B"
+    },
+    {
+        num: "03",
+        dest: "Hawaii",
+        date: "02.08 / 02.17"
+    },
+    {
+        num: "04",
+        dest: "Fiji",
+        date: "02.17 / 02.27"
+    },
+    {
+        num: "05",
+        dest: "New Zealand",
+        date: "02.27 / 03.08"
+    },
+    {
+        num: "06",
+        dest: "Australia",
+        date: "03.08 / 03.21"
+    },
+    {
+        num: "07",
+        dest: "Bali",
+        date: "03.21 / 03.29"
+    },
+    {
+        num: "08",
+        dest: "Singapore",
+        date: "03.29 / 04.05"
+    },
+    {
+        num: "09",
+        dest: "Thailand",
+        date: "04.05 / 04.25"
+    },
+    {
+        num: "10",
+        dest: "Philippines",
+        date: "04.25 / 05.04"
+    },
+    {
+        num: "11",
+        dest: "Shanghai",
+        date: "05.04 / 05.10"
+    },
+    {
+        num: "12",
+        dest: "Japan",
+        date: "05.10 / 05.30"
+    },
+    {
+        num: "13",
+        dest: "Budapest",
+        date: "05.31 / -"
+    }
+]
